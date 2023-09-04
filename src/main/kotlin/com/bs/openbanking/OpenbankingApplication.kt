94977b70f464c7a9d2795af3ced42ae6dcdf5824
beomsun1234 <beomsun22@gmail.com>
2023-09-04 20:29:36 +0900

    Feat: #1 프로젝트 세팅

---
+++ src/main/kotlin/com/bs/openbanking/OpenbankingApplication.kt
@@ -0,0 +1,11 @@
+package com.bs.openbanking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenbankingApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenbankingApplication>(*args)
+}
