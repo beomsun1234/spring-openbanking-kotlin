69244456a257ad7ef9d324877348a4b790e8abf7
beomsun1234 <beomsun22@gmail.com>
2023-09-06 21:15:21 +0900

    feat: @EnableJpaAuditing 추가

--- src/main/kotlin/com/bs/openbanking/OpenbankingApplication.kt
+++ src/main/kotlin/com/bs/openbanking/OpenbankingApplication.kt
@@ -2,8 +2,10 @@ package com.bs.openbanking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class OpenbankingApplication
 
 fun main(args: Array<String>) {
