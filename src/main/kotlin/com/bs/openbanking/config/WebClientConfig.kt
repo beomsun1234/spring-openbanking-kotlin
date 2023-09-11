fc630918286eb4c0add7b42d59e8365f84acb1a3
beomsun1234 <beomsun22@gmail.com>
2023-09-11 23:25:32 +0900

    feat: webclient config 추가

---
+++ src/main/kotlin/com/bs/openbanking/config/WebClientConfig.kt
@@ -0,0 +1,27 @@
+package com.bs.openbanking.config
+
+import io.netty.handler.ssl.SslContextBuilder
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
+
+@Configuration
+class WebClientConfig {
+    @Bean
+    fun webClient():WebClient{
+        val httpClient = HttpClient.create()
+            .responseTimeout(Duration.ofSeconds(2))
+            .secure { it.sslContext(
+                SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build())
+            }
+        return WebClient.builder()
+            .clientConnector(ReactorClientHttpConnector(httpClient))
+            .build()
+    }
+}
