dfe47ce32c0cbd3eb1cdcf65c3082ae32298f456
beomsun1234 <beomsun22@gmail.com>
2023-09-14 23:27:05 +0900

    feat: 오픈뱅키 정보 저장 update 쿼리 추가

--- src/main/kotlin/com/bs/openbanking/repository/MemberRepository.kt
+++ src/main/kotlin/com/bs/openbanking/repository/MemberRepository.kt
@@ -2,11 +2,24 @@ package com.bs.openbanking.repository
 
 import com.bs.openbanking.domain.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.util.Optional
 
 @Repository
 interface MemberRepository:JpaRepository<Member, Long> {
     fun existsMemberById(id:Long):Boolean
     fun findMemberByEmail(email:String):Optional<Member>
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Member set openBankId = :openBankId where id = :id")
+    fun updateOpenBankId(id:Long,openBankId:String)
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Member set openBankCi = :openBankCi where id = :id")
+    fun updateOpenBankCi(id:Long,openBankCi:String)
 }
