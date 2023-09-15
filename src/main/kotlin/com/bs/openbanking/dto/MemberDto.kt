d5945e35b70c83e8fbd6cb3fe4f4064fbccf20ab
beomsun1234 <beomsun22@gmail.com>
2023-09-15 22:09:32 +0900

    feat: member dto 오픈뱅킹 id 추가

--- src/main/kotlin/com/bs/openbanking/dto/MemberDto.kt
+++ src/main/kotlin/com/bs/openbanking/dto/MemberDto.kt
@@ -5,10 +5,11 @@ import com.bs.openbanking.domain.Member
 data class MemberDto(
     val memberId:Long?=null,
     val email:String?=null,
+    val openBankId:String?=null,
 ){
     companion object{
         fun from(member:Member):MemberDto{
-            return MemberDto(member.id, member.email)
+            return MemberDto(member.id, member.email, openBankId = member.openBankId)
         }
     }
 }
